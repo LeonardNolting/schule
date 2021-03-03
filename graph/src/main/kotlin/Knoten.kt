694ae62a3d60b33e5209ef83d00a797d475bb1ec
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-03-03 09:06:13 +0100

    Knoten.getBezeichnung füllt mit Leerzeichen bis zu Breite auf

--- graph/src/main/kotlin/Knoten.kt
+++ graph/src/main/kotlin/Knoten.kt
@@ -1,5 +1,5 @@
 class Knoten(
 	private val bezeichnung: String
 ) {
-	fun getBezeichnung(breite: UInt) = bezeichnung.substring(0 until breite.toInt())
+	fun getBezeichnung(breite: UInt) = bezeichnung.substring(0 until breite.toInt()).padEnd(breite.toInt(), ' ')
 }
