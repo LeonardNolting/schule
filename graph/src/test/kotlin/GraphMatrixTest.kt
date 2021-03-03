50c38dbc35e769c2e2956ff05e0a240db2d7cbaf
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-03-03 09:33:28 +0100

    GraphMatrixTest nach Buch S.97 Abb. 3

--- graph/src/test/kotlin/GraphMatrixTest.kt
+++ graph/src/test/kotlin/GraphMatrixTest.kt
@@ -2,9 +2,13 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 internal class GraphMatrixTest {
+	val matrix = GraphMatrix(maxKnoten = 14u)
+	val staedte = listOf("F", "KA", "FD", "WÜ", "S", "UL", "LI", "HO", "N", "A", "R", "M", "PA", "RO")
 
 	@BeforeEach
 	fun setUp() {
+		for (stadt in staedte)
+			matrix.fuegeKnotenEin(stadt)
 	}
 
 	@Test
