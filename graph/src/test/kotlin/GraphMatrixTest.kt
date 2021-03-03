d1c64abc6f5256de98956b49e04a0b6100a4d48f
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-03-03 09:25:35 +0100

    Matrix Test

---
+++ graph/src/test/kotlin/GraphMatrixTest.kt
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class GraphMatrixTest {
+
+	@BeforeEach
+	fun setUp() {
+	}
+
+	@Test
+	fun fuegeKnotenEin() {
+	}
+
+	@Test
+	fun getKantengewicht() {
+	}
+}
